89ada0d91902a68220b1c9e03654cca941cfb2ed
Valeriy Morkovin <minouts@gmail.com>
2015-07-10 16:50:40 +0300

    init node chat

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp'),
-    jade = require('gulp-jade'),
     stylus = require('gulp-stylus'),
     livereload = require('gulp-livereload'),
     myth = require('gulp-myth'),
@@ -9,52 +8,16 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     connect = require('connect'),
     addsrc = require('gulp-add-src'),
+    server = require('gulp-express'),
     coffee = require('gulp-coffee');
 
 
-gulp.task('stylus', function() {
-    gulp.src('./assets/stylus/screen.styl')
-        .pipe(stylus({
-            use: ['nib']
-        }))
-    .on('error', console.log)
-    .pipe(myth())
-    .pipe(gulp.dest('./public/css/'))
-});
-
-
-gulp.task('jade', function() {
-    gulp.src(['./assets/template/*.jade', '!./assets/template/_*.jade'])
-        .pipe(jade({
-            pretty: true
-        }))
-        .on('error', console.log)
-    .pipe(gulp.dest('./public/'))
-});
-
-
-gulp.task('js', function() {
-    gulp.src(['./assets/coffee/**/*.coffee', '!./assets/coffee/vendor/**/*.coffee'])
-        .pipe(coffee()).on('error', console.log)
-        .pipe(addsrc(['./assets/js/**/*.js', '!./assets/coffee/vendor/**/*.js']))
-        .pipe(concat('index.js'))
-        .pipe(gulp.dest('./public/js'))
-});
-
-
 gulp.task('build', function() {
-    gulp.src('./assets/stylus/screen.styl')
-        .pipe(stylus({
-            use: ['nib']
-        }))
-    .pipe(myth())
-    .pipe(csso())
-    .pipe(gulp.dest('./build/css/'))
-
-    // jade
-    gulp.src(['./assets/template/*.jade', '!./assets/template/_*.jade'])
-        .pipe(jade())
-        .pipe(gulp.dest('./build/'))
+    gulp.src('./assets/stylus/*.styl')
+        .pipe(stylus())
+        .pipe(myth())
+        .pipe(csso())
+        .pipe(gulp.dest('./public/stylesheets/'))
 
     // js
     gulp.src(['./assets/coffee/**/*.coffee', '!./assets/coffee/vendor/**/*.coffee'])
@@ -62,19 +25,34 @@ gulp.task('build', function() {
         .pipe(addsrc(['./assets/js/**/*.js', '!./assets/coffee/vendor/**/*.js']))
         .pipe(concat('index.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('./build/js'));
+        .pipe(gulp.dest('./public/javascripts'));
 
     // image
     gulp.src('./assets/img/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('./build/img'))
+        .pipe(gulp.dest('./public/images'))
 
 });
 
 
-gulp.task('images', function() {
-    gulp.src('./assets/img/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./public/img'))
+gulp.task('server', function () {
+    // Start the server at the beginning of the task
+    server.run(['./bin/www']);
 
+    // Restart the server when file changes
+    gulp.watch(['./views/**/*.ejs'], server.notify);
+    gulp.watch(['./public/stylesheets/**/*.css'], ['styles:css']);
+    gulp.watch(['{.tmp,./public}/stylesheets/**/*.css'], function(event){
+        gulp.run('styles:css');
+        server.notify(event);
+    });
+
+    gulp.watch(['./public/javascripts/**/*.js'], ['jshint']);
+    gulp.watch(['./public/images/**/*'], server.notify);
+    gulp.watch(['app.js', 'routes/**/*.js'], [server.run]);
+});
+
+gulp.task('stop', function () {
+    // Start the server at the beginning of the task
+    server.stop();
 });
